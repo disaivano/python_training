1c2b7546fbd30dcbb62a34067c96572e8fc3c760
disaivano <ivanovdenis2@mail.ru>
2017-03-05 15:05:32 +0200

    Выделены вспомогательные методы и классы

--- test_add_group1.py
+++ test_add_group1.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
-from selenium.webdriver.firefox.webdriver import WebDriver
 import unittest
+
+from selenium.webdriver.firefox.webdriver import WebDriver
+
+from group import Group
+
 
 def is_alert_present(wd):
     try:
@@ -14,12 +18,22 @@ class test_add_group1(unittest.TestCase):
         self.wd = WebDriver()
         self.wd.implicitly_wait(60)
     
-    def test_test_add_group1(self):
+    def test_add_group1(self):
         wd = self.wd
         self.open_home_page(wd)
-        self.login(wd)
+        self.login(wd, username="admin", password="secret")
         self.init_group_creation(wd)
-        self.fill_the_form(wd)
+        self.fill_the_form(wd, Group(firstname="new group", middlename="1", lastname="1", nickname="1", title="1", company="1"))
+        self.submit_group_creation(wd)
+        self.logout(wd)
+
+    def test_add_empty_group1(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.init_group_creation(wd)
+        self.fill_the_form(wd, Group(firstname="", middlename="", lastname="", nickname="", title="",
+                           company=""))
         self.submit_group_creation(wd)
         self.logout(wd)
 
@@ -33,41 +47,41 @@ class test_add_group1(unittest.TestCase):
     def submit_group_creation(self, wd):
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
 
-    def fill_the_form(self, wd):
+    def fill_the_form(self, wd, group):
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys("new group")
+        wd.find_element_by_name("firstname").send_keys(group.firstname)
         wd.find_element_by_name("middlename").click()
         wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys("1")
+        wd.find_element_by_name("middlename").send_keys(group.middlename)
         wd.find_element_by_name("lastname").click()
         wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys("1")
+        wd.find_element_by_name("lastname").send_keys(group.lastname)
         wd.find_element_by_name("nickname").click()
         wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys("1")
+        wd.find_element_by_name("nickname").send_keys(group.nickname)
         wd.find_element_by_name("title").click()
         wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys("1")
+        wd.find_element_by_name("title").send_keys(group.title)
         wd.find_element_by_name("company").click()
         wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys("1")
+        wd.find_element_by_name("company").send_keys(group.company)
         wd.find_element_by_name("address").click()
 
     def init_group_creation(self, wd):
         wd.find_element_by_link_text("add new").click()
 
-    def login(self, wd):
+    def login(self, wd, username, password):
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").send_keys("\\undefined")
         wd.find_element_by_id("LoginForm").click()
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_id("LoginForm").click()
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
 
     def open_home_page(self, wd):
