12324e3d295cc688df503ab733f31d6dc97212b3
disaivano <ivanovdenis2@mail.ru>
2017-03-05 09:52:04 +0200

    Добавил успешный тест, убрал неуспешный

--- test_add_group1.py
+++ test_add_group1.py
@@ -10,18 +10,18 @@ def is_alert_present(wd):
     except:
         return False
 
-class test_add_group(unittest.TestCase):
+class test_add_group1(unittest.TestCase):
     def setUp(self):
         self.wd = WebDriver()
         self.wd.implicitly_wait(60)
     
-    def test_test_add_group(self):
+    def test_test_add_group1(self):
         success = True
         wd = self.wd
         wd.get("http://localhost/addressbook/")
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").send_keys("\\undefined")
-        wd.find_element_by_css_selector("ul").click()
+        wd.find_element_by_id("LoginForm").click()
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys("admin")
@@ -33,18 +33,24 @@ class test_add_group(unittest.TestCase):
         wd.find_element_by_link_text("add new").click()
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys("привет")
+        wd.find_element_by_name("firstname").send_keys("new group")
         wd.find_element_by_name("middlename").click()
         wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys("вапывпывп")
+        wd.find_element_by_name("middlename").send_keys("1")
         wd.find_element_by_name("lastname").click()
         wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys("ывпып")
+        wd.find_element_by_name("lastname").send_keys("1")
+        wd.find_element_by_name("nickname").click()
+        wd.find_element_by_name("nickname").clear()
+        wd.find_element_by_name("nickname").send_keys("1")
+        wd.find_element_by_name("title").click()
+        wd.find_element_by_name("title").clear()
+        wd.find_element_by_name("title").send_keys("1")
+        wd.find_element_by_name("company").click()
+        wd.find_element_by_name("company").clear()
+        wd.find_element_by_name("company").send_keys("1")
+        wd.find_element_by_name("address").click()
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
-        wd.find_element_by_link_text("home page").click()
-        if not wd.find_element_by_id("2").is_selected():
-            wd.find_element_by_id("2").click()
-        wd.find_element_by_xpath("//div[@id='content']/form[2]/div[2]/input").click()
         wd.find_element_by_link_text("Logout").click()
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").send_keys("\\undefined")
